7b44ea58e2f4a61af240301f10d8f943c1c577e9
Nav <navdeep_aulakh24@hotmail.com>
2023-02-16 18:57:55 -0500

    finished htmlroutes.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const apiroutes = require('./routes/apiroutes');
+const htmlroutes = require('./routes/htmlroutes');
 const app = express();
 
 const PORT = process.env.PORT || 3001;
@@ -6,9 +8,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/', htmlroutes);
+app.use('/api', apiroutes);
 
-require('./routes/apiroutes')(app);
-require('./routes/htmlroutes')(app);
+// require('./routes/apiroutes')(app);
+// require('./routes/htmlroutes')(app);
 
 app.listen(PORT, () => {
     console.log(`Server available at localhost${PORT}`);
